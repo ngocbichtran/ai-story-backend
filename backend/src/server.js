c79f0bc0ebc0c3c1367beeb1d6c8852e6c7f0cc7
NgocBich <ngocbich@gmail.com>
2026-05-25 18:57:36 +0700

    change port

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
